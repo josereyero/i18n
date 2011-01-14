55b2e6a1ad68a0d7a1148d16da1bdfb69d43e958
Jose Reyero <jose@developmentseed.org>
2011-01-14 12:00:22 +0100

    Drop old permissions, change to administer content translation

---
+++ i18n_block/i18n_block.js
@@ -0,0 +1,31 @@
+// $Id: i18n_block.js,v 1.16 2010/05/09 13:57:59 dries Exp $
+
+(function ($) {
+
+/**
+ * Provide the summary information for the block settings vertical tab.
+ */
+Drupal.behaviors.i18nSettingsSummary = {
+  attach: function (context) {
+
+    $('fieldset#edit-languages', context).drupalSetSummary(function (context) {
+      var summary = '';
+      if ($('.form-item-i18n-mode input[type=checkbox]:checked', context).val()) {
+        summary += Drupal.t('Translatable');
+      }
+      else {
+        summary += Drupal.t('Not translatable');
+      }
+      summary += ', ';
+      if ($('.form-item-languages input[type=checkbox]:checked', context).val()) {
+        summary += Drupal.t('Restricted to certain languages');
+      }
+      else {
+        summary += Drupal.t('Not restricted');
+      }
+      return summary;
+    });
+  }
+};
+
+})(jQuery);
