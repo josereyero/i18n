7d26d8821956d69ea2f10deef3de70967f057820
rvilar <ramon@ymbra.com>
2011-02-04 17:54:34 +0100

    Adapted JS file to D7

--- i18n_node/i18n_node.js
+++ i18n_node/i18n_node.js
@@ -1,4 +1,5 @@
 // $Id: i18n.js,v 1.1.2.3 2009/01/20 20:35:55 nedjo Exp $
+(function ($) {
 
 /**
  * Rewrite autocomplete inputs to pass the language of the node currently being
@@ -7,11 +8,14 @@
  * This functionality ensures node autocompletes get suggestions for the node's
  * language rather than the current interface language.
  */
-Drupal.behaviors.i18n = function (context) {
-  if (Drupal.settings && Drupal.settings.i18n) {
-    $('form[id^=node-form]', context).find('input.autocomplete[value^=' + Drupal.settings.i18n.interface_path + ']').each(function () {
-      $(this).val($(this).val().replace(Drupal.settings.i18n.interface_path, Drupal.settings.i18n.content_path));
-    });
+Drupal.behaviors.i18n = {
+  attach : function (context) {
+    if (Drupal.settings && Drupal.settings.i18n) {
+      $('form[id^=node-form]', context).find('input.autocomplete[value^=' + Drupal.settings.i18n.interface_path + ']').each(function () {
+        $(this).val($(this).val().replace(Drupal.settings.i18n.interface_path, Drupal.settings.i18n.content_path));
+      });
+    }
   }
 };
 
+})(jQuery);
